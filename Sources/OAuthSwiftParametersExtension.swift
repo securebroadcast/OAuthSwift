9455541b199787a94b48bd569cc6e71ceb046c50
Silvia Pititto <26574373+silvia-pt@users.noreply.github.com>
2019-09-12 16:02:39 +0100

    Now setting the OAuth header correctly.

---
+++ Sources/OAuthSwiftParametersExtension.swift
@@ -0,0 +1,9 @@
+//
+//  OAuthSwiftParametersExtension.swift
+//  OAuthSwift
+//
+//  Created by Silvia Pititto on 12/09/2019.
+//  Copyright © 2019 Dongri Jin. All rights reserved.
+//
+
+import Foundation
